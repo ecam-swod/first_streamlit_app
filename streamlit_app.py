53b5d13f03db6a1495736846e9ac584ab7a687ba
ecam-swod <120137684+ecam-swod@users.noreply.github.com>
2022-12-09 12:10:38 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-# fruit_choice_2 = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice_2 = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('Thanks for entering ', fruit_choice_2)
